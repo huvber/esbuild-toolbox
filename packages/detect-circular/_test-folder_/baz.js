0bfd3baa201046858562fc4271a6119cdb432b36
huvber <huvber@gmail.com>
2026-05-08 23:31:39 +0200

    feat: add detect-circular plugin

---
+++ packages/detect-circular/_test-folder_/baz.js
@@ -0,0 +1,5 @@
+import { foo } from "./index";
+
+export const baz = () => {
+  foo();
+};
---
+++ packages/detect-circular/plugin.spec.js
@@ -0,0 +1,42 @@
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { test, describe } from "node:test";
+import { dirname } from "node:path";
+import assert from "node:assert";
+
+import { build } from "esbuild";
+
+import { detectCircularPlugin } from "./plugin.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+describe("detectCircularPlugin", async () => {
+  test("should detect circular dependencies", async () => {
+    const result = await build({
+      entryPoints: ["./_test-folder_/index.js"],
+      bundle: true,
+      plugins: [detectCircularPlugin()],
+      outdir: "./dist",
+    });
+
+    assert.equal(result.errors.length, 1);
+    assert.equal(result.warnings.length, 0);
+  });
+
+  test("should only warn about circular dependencies", async () => {
+    const result = await build({
+      entryPoints: ["./_test-folder_/index.js"],
+      bundle: true,
+      plugins: [
+        detectCircularPlugin({
+          logLevel: "warn",
+        }),
+      ],
+      outdir: "./dist",
+    });
+
+    assert.equal(result.errors.length, 0);
+    assert.equal(result.warnings.length, 1);
+  });
+});
